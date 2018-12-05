ce8b9edb01a6732b38b9298f391b9e8a2e12c0b3
ZinoDzYT <45629956+zinobs@users.noreply.github.com>
2018-12-05 21:22:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -279,19 +279,49 @@ client.on("guildMemberAdd", member => {
 
 
 
-client.on('guildMemberAdd', member => { //LAST CODES -HONRAR-
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "mirage");
-     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
-  });
-}) 
+
  
  
- 
+ client.on('message', message => {
+              if(!message.channel.guild) return;
+    if(message.content.startsWith('Zbc')) {
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );  //7md
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+    let copy = "DgPro-BC";
+    let request = `Requested By ${message.author.username}`;  //7md
+    if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+    msg.react('✅')
+    .then(() => msg.react('❌'))  //7md
+    .then(() =>msg.react('✅'))  //7md
+
+
+
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+       let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });  //7md
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });  //7md
+    reaction1.on("collect", r => {
+    message.channel.send(`☑ |   ${message.guild.members.size} يتم ارسال البرودكاست الى عضو `).then(m => m.delete(5000));  //7md
+    message.guild.members.forEach(m => {
+    var bc = new  
+       Discord.RichEmbed()
+       .setColor('RANDOM')
+       .setTitle('البرودكاست') .addField('السيرفر', message.guild.name) .addField('المرسل', message.author.username)  //7md
+       .addField('الرساله', args)  //7md
+       .setThumbnail(message.author.avatarURL)  //7md
+       .setFooter(copy, client.user.avatarURL); //7md
+    m.send({ embed: bc })
+    msg.delete();  //7md
+    })
+    })
+    reaction2.on("collect", r => {  //7md
+    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));   //7md
+    msg.delete();  //7md  
+    })  //7md
+    }) //7md
+    }  //7md
+    }) //7md
    
  
 client.login(process.env.BOT_TOKEN);
